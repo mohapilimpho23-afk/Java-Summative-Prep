a4d782dec43bf83d4cb4ac8bd2709bca3138b803
nkmorjhb025 <nkmorjhb025@student.wethinkcode.co.za>
2026-06-02 17:23:45 +0200

    Added shield repair class

---
+++ src/main/java/summative/ShieldRepairTask.java
@@ -0,0 +1,16 @@
+package summative;
+
+// Question 3
+
+public class ShieldRepairTask { // Implement the correct interface
+
+    private final Robot robot;
+    private final long repairTimeMs;
+
+    public ShieldRepairTask(Robot robot, long repairTimeMs) {
+        this.robot = robot;
+        this.repairTimeMs = repairTimeMs;
+    }
+
+    // Implement required thread execution method
+}
